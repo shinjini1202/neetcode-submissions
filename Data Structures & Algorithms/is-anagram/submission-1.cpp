b96f9cd2044fb5e8e41100d2230b56ae563dd619
Shinjini Nag <125287325+shinjini1202@users.noreply.github.com>
2026-08-18 16:34:35 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size()!=t.size()) return false;
+        int freq[26]={0};
+        for (int i=0;i<s.size();i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for (int f : freq){
+            if (f!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
