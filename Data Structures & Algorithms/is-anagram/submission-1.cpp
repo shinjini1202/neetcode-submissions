8cd5116e0c12d8b65dbe3d5a3fe1999e50c4acb9
Shinjini Nag <125287325+shinjini1202@users.noreply.github.com>
2026-08-18 16:38:09 +0530

    Fix frequency array type to int for anagram check
    Changed frequency array type from char to int to prevent overflow issues.

--- Data Structures & Algorithms/is-anagram/submission-1.cpp
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size()!=t.size()) return false;
-        int freq[26]={0};
+        int freq[26]={0};        //8 bit integer overflow gives wrong answer if freq array is declared is "char" instead of "int"
         for (int i=0;i<s.size();i++){
             freq[s[i] - 'a']++;
             freq[t[i] - 'a']--;
